174623675636db8e820537fdf3999c6cd944f558
OceanSource <oceansource@gmail.com>
2016-10-29 07:09:05 -0700

    Add fixtures service to collection

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,10 +1,7 @@
 (function() {
     function CollectionCtrl(Fixtures) {
-		this.albums = [];
-			for (var i=0; i < 2; i++) {
-				this.album = Fixtures.getAlbum();
-				this.albums.push(this.album);
-			}
+		
+		this.albums = Fixtures.getCollection(12);
     }
  
      angular
